6437595a6dfa0d4964e8b3699cf27b4d6cc51d34
riyasachdeva04 <rsachdeva_be22@thapar.edu>
2023-09-12 16:23:05 +0530

    bisection

---
+++ bisection-method.m
@@ -0,0 +1,27 @@
+a = input('lower limit');
+b = input('upper limit');
+
+tol = input('tolerance');
+n = input('iterations');
+
+f = @(x) x*x-x-12;
+
+
+if f(a)*f(b)>0
+    disp('Root does not exist in this interval');
+else
+
+    for i=1:n
+        c = (a+b)/2;
+        if f(a)*f(c)<0
+            b = c;
+        else
+            a = c;
+        end
+        
+        if abs(a-b)<=tol
+            break
+        end
+    end
+    disp(c);
+end
